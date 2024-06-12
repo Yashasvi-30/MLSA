05f2b8bd940cc2eac46f7ef1b73d725b782b43e4
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2024-06-12 22:43:17 +0530

    Upadte Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "__images__/imagebg.jpg";
+import image from "../../images/imagebg.jpg";
 
-const imageAltText = "soft scenery image background";
+const imageAltText = "soft scenery of mountains image background";
 
 const Home = ({ name, title }) => {
   return (
