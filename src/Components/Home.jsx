343c3aba6c4809b6766425997793827082ca0399
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2024-06-12 22:58:31 +0530

    hii hello

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "/Users/yashasvigupta/Documents/GitHub/MLSA/__images__/imagebg.jpg";
+import image from "../images/woman-with-tablet.jpg";
 
 const imageAltText = "soft scenery of mountains image background";
 
