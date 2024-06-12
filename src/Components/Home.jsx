76899417389a8de2149546b537f941d5fe8839e4
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2024-06-12 22:34:35 +0530

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/imagebg.jpg";
+import image from "__images__/imagebg.jpg";
 
 const imageAltText = "soft scenery image background";
 
