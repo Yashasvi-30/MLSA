e88869abe76ae112bbaeac537efd50b4b3352a74
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2024-06-13 00:49:12 +0530

    Breakdown moment

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/imagebg.jpg";
+import image from "src/images/imagebg.jpg";
 
 const imageAltText = "soft scenery of mountains image background";
 
